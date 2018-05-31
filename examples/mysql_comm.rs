f25adc9a3a37419a5c4682a2281ebbdc7fc762b9
hanguang <hanguang@pyyx.com>
2018-05-31 15:56:34 +0800

    refactor(mysql配置): 优化获取MysqlConfig结构体

--- examples/mysql_comm.rs
+++ examples/mysql_comm.rs
@@ -1,5 +1,5 @@
 extern crate rust_db;
-use rust_db::config::mysql::Mysql as MysqlConfig;
+use rust_db::config::mysql::MysqlConfig;
 use std::process;
 
 
@@ -7,17 +7,13 @@ extern crate mysql;
 use mysql as my;
 
 fn main() {
-    let config = MysqlConfig::new().unwrap_or_else(|err| {
+    let opts = MysqlConfig::new().unwrap_or_else(|err| {
         println!("ini文件有误: {}", err);
         process::exit(1);
     });
-    let comm = config.driver + "://" + &config.username + ":" +
-    	&config.password + "@" + &config.host + ":" + &config.port.to_string();
-    let pool = my::Pool::new(comm).unwrap_or_else(|err| {
+    let pool = my::Pool::new(opts).unwrap_or_else(|err| {
         println!("mysql连接失败: {}", err);
         process::exit(1);
     });
     println!("{:?}", pool);
-
-
 }
--- src/config/mysql.rs
+++ src/config/mysql.rs
@@ -1,59 +1,43 @@
 extern crate ini;
+extern crate mysql;
 
 use self::ini::Ini;
+use self::mysql::{Opts,OptsBuilder};
 use config::config::*;
 use config::env::get_env;
 
-#[derive(Debug)]
-pub struct Mysql {
-    pub driver: String,
-    pub host: String,
-    pub port: u16,
-    pub username: String,
-    pub password: String,
-    pub database: String,
+pub struct MysqlConfig;
 
-}
+impl MysqlConfig{
+    pub fn new () -> Result<Opts, &'static str> {
+        let mut builder = OptsBuilder::new();
 
-impl Mysql {
-    pub fn new () -> Result<Mysql, &'static str> {
-        let section_name = get_env() + ":common";
-        let conf = Ini::load_from_file(get_config(Conf::Mysql)).unwrap();
-        let section = match conf.section(Some(section_name.to_owned())) {
+        let _section_name = get_env() + ":common";
+        let _conf = Ini::load_from_file(get_config(Conf::Mysql)).unwrap();
+        let _section = match _conf.section(Some(_section_name.to_owned())) {
             Some(arg) => arg,
             None => return Err("section value is null"),
         };
-        let driver = match section.get("a.driver") {
-            Some(arg) => arg.to_string(),
-            None => return Err("driver value is null"),
-        };
-        let host = match section.get("a.host") {
-            Some(arg) => arg.to_string(),
+        match _section.get("a.host") {
+            Some(arg) => builder.ip_or_hostname(Some(arg.to_string())),
             None => return Err("host value is null"),
         };
-        let port = match section.get("a.port") {
-            Some(arg) => arg.parse::<u16>().unwrap(),
+        match _section.get("a.port") {
+            Some(arg) => builder.tcp_port(arg.parse::<u16>().unwrap()),
             None => return Err("port value is null"),
         };
-        let username = match section.get("a.username") {
-            Some(arg) => arg.to_string(),
+        match _section.get("a.username") {
+            Some(arg) => builder.user(Some(arg.to_string())),
             None => return Err("username value is null"),
         };
-        let password = match section.get("a.password") {
-            Some(arg) => arg.to_string(),
+        match _section.get("a.password") {
+            Some(arg) => builder.pass(Some(arg.to_string())),
             None => return Err("password value is null"),
         };
-        let database = match section.get("a.database") {
-            Some(arg) => arg.to_string(),
+        match _section.get("a.database") {
+            Some(arg) => builder.db_name(Some(arg.to_string())),
             None => return Err("database value is null"),
         };
-        Ok(Mysql {
-            driver,
-            host,
-            port,
-            username,
-            password,
-            database,
-        })
+        Ok(builder.into())
     }
 }
